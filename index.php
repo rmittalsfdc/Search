c36f8e399514bce323c356a2a4fbb3c3d53fb6a3
rmittalsfdc <rmittalsfdc@gmail.com>
2015-07-17 18:22:42 +0530

    1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $output = "<div>No Result found.</div>";
 if(isset($_POST['search']) && !empty($_POST['search'])){
 	$searchq = trim($_POST['search']);
-	//$searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+	$searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
 	$dbname = "df8k1m58fmo0qg";
 	$host = "ec2-54-83-36-176.compute-1.amazonaws.com";
 	$port = 5432;
@@ -12,7 +12,7 @@ if(isset($_POST['search']) && !empty($_POST['search'])){
 
 	$dbdrv=new PostgreDB ($dbname, $host, $port, $user, $password, $persistent);
 	$dbdrv->Begin();
-	$sql= "SELECT * FROM company Where First_Name LIKE LOWER('%$searchq%') OR Last_Name LIKE LOWER('%$searchq%')";
+	$sql= "SELECT * FROM company Where LOWER(First_Name) LIKE LOWER('%$searchq%') OR LOWER(Last_Name) LIKE LOWER('%$searchq%')";
 	if (!$dbdrv->ExecQuery($sql)){
 	    die ($dbdrv->Error());
 	}
