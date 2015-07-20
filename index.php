fb0b09ac70a0d542c7c604ed0d7062f9b964df6b
rmittalsfdc <rmittalsfdc@gmail.com>
2015-07-20 14:14:30 +0530

    insert function

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php include('postgredb.php');
 $output = "<div>No Result found.</div>";
-if(isset($_POST['search']) && !empty($_POST['search'])){
-	$searchq = trim($_POST['search']);
+if((isset($_POST['ifirstname']) && !empty($_POST['ifirstname'])) || (isset($_POST['ilastname']) && !empty($_POST['ilastname'])) || (isset($_POST['iage']) && !empty($_POST['iage'])) || (isset($_POST['iaddress']) && !empty($_POST['iaddress']))){
+	$searchfirstname = trim($_POST['ifirstname']);
+	$searchlastname = trim($_POST['ilastname']);
+	$searchage = trim($_POST['iage']);
+	$searchaddress = trim($_POST['iaddress']);
 	//$searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
 	$dbname = "df8k1m58fmo0qg";
 	$host = "ec2-54-83-36-176.compute-1.amazonaws.com";
@@ -12,19 +15,36 @@ if(isset($_POST['search']) && !empty($_POST['search'])){
 
 	$dbdrv=new PostgreDB ($dbname, $host, $port, $user, $password, $persistent);
 	$dbdrv->Begin();
-	$sql= "SELECT * FROM company Where LOWER(First_Name) LIKE LOWER('%$searchq%') OR LOWER(Last_Name) LIKE LOWER('%$searchq%')";
+	$sql= "SELECT * FROM company Where LOWER(First_Name) LIKE LOWER('%$searchfirstname%') OR LOWER(Last_Name) LIKE LOWER('%$searchlastname%') OR LOWER(Age) LIKE LOWER('%$searchage%') OR LOWER(Address) LIKE LOWER('%$searchaddress%')";
 	if (!$dbdrv->ExecQuery($sql)){
 	    die ($dbdrv->Error());
 	}
-	
+	$query = "INSERT INTO company(first_name, last_name, age, address) VALUES('" . $searchfirstname . "', '" . $searchlastname . "', '" . $searchage . "', '" . $searchaddess . "')";
+	if (!$dbdrv->ExecQuery($query)){
+		die ($dbdrv->Error());
+	}
+	else{
+		echo "Record Inserted";
+	}
 }
 ?>
 
 <form action="index.php" method="post">
-	<input type="text" name="search" placeholder="Search for members" />
+	<p><label for="ifirstname">First Name:</label> 
+	<input type="text" name="ifirstname" placeholder="Search for first name" id="ifirstname"/>
+	
+	<label for="ilastname">Last Name:</label> 
+	<input type="text" name="ilastname" placeholder="Search for last name" id="ilastname"/>
+	
+	<label for="iage">Age:</label> 
+	<input type="text" name="iage" placeholder="Search for age" id="iage"/>
+	
+	<label for="iaddress">Address:</label> 
+	<input type="text" name="iaddress" placeholder="Search for address" id="iaddress"/>
 	<input type="submit" value="Search" /> 
+	<input type="submit" value="Insert" /> 
+	</p>
 </form>
-
 <table border="1" style="width:100%">
 <tr>
    <th>First Name</th>
@@ -47,7 +67,7 @@ if($dbdrv->NumRows() > 0){
 	<td align="center"><?php echo $result["address"]?></td>
  </tr>
 		
-		<?php
+<?php
 	}
 		
 	}
